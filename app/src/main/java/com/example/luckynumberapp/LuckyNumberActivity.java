5676c1a978f77c74f3220b361cdf53a6994dd2a9
jose-omar-landa <jlanda4@wgu.edu>
2023-03-31 18:38:00 -0500

    Added functionality to share the generated lucky number through the user's messaging app of choice.

--- app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
+++ app/src/main/java/com/example/luckynumberapp/LuckyNumberActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +33,14 @@ public class LuckyNumberActivity extends AppCompatActivity {
         //Random number generator
         int random_number = generateRandomNum();
         luckyNumberText.setText(""+random_number);
+
+        //Shares data when share button clicked
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareData(user_name, random_number);
+            }
+        });
     }
 
     public int generateRandomNum() {
@@ -39,6 +48,19 @@ public class LuckyNumberActivity extends AppCompatActivity {
         int upperLimit = 1000;
         int generatedNumber = random.nextInt(upperLimit);
         return generatedNumber;
+    }
+
+    public void shareData(String username, int randomNum){
+        //Implicit Intents
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+
+        String number = String.valueOf(randomNum);
+
+        i.putExtra(Intent.EXTRA_SUBJECT, username + " has a lucky number!");
+        i.putExtra(Intent.EXTRA_TEXT, "Your friend " + username + "'s lucky number is " + number + "!");
+
+        startActivity(Intent.createChooser(i, "Choose A Platform"));
     }
 
 }
